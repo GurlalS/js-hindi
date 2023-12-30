4da370bc7885cfef9e042dfe8a2e5189e63963a6
GurlalS <121489814+GurlalS@users.noreply.github.com>
2023-12-30 20:08:56 +0000

    ControlFlows

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,35 @@
+// switch case concept =>. alternate to if elseif else
+
+// syntax
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+
+// Note: if break is is missed in any case and case case got executed, it will execute rest all the cases until it encounters break or default. default will not get executed
